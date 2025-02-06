75c7279b5fbd2f6d98b4c8e36e5d347afd17de32
unknown <starwwind@gmail.com>
2025-02-06 23:09:17 +0700

    start

---
+++ main.js
@@ -0,0 +1,67 @@
+// const a = 6;
+// let square = a * a;
+// console.log(`Площадь квадрата со стороной ${a} равна ${square}`);
+
+// const radius = 10;
+// let result = Math.PI * radius ** 2 ;
+// console.log(`Радиус: ${radius}
+// Площадь: ${result.toFixed(2)}`);
+
+// const apples = 10;
+// const people = 3;
+// let count = apples / people;
+// let remainder = apples % people;
+// console.log(`Каждый получит по ${count.toFixed(0)} яблока`);
+// console.log(`Останется ${remainder}`);
+
+// const classes = 13;
+// const students = 7;
+// let countDeskInClass = Math.ceil(students / 2);
+// let buyDesk = countDeskInClass * classes;
+// console.log(`Классов: ${classes}`);
+// console.log(`Учеников: ${students}`);
+// console.log(`Нужно купить парт: ${buyDesk}`);\
+
+// let i = 0;
+//
+// console.log('1:', i); //0
+//
+// console.log('2:', i++); //0
+// console.log('3:', i); //1
+//
+// console.log('4:', ++i); //2
+// console.log('5:', i); //2
+//
+// console.log('6:', i--); //2
+// console.log('7:', i); //1
+//
+// console.log('8:', --i); //0
+// console.log('9:', i); //0
+
+
+// let a = 10;
+//
+// console.log(1 > 0); //true
+// console.log(!(1 < 0)); //true
+//
+// console.log('a' == a); //true
+// console.log('a' !== a); //false
+//
+// a = 4;
+//
+// console.log(a == 10); //false
+// console.log(a == '10'); //false
+//
+// console.log(a >= 4); //true
+// console.log('4' == a); //true
+//
+// console.log(!(1 === '1')); //true
+//
+// console.log(!!true); //true
+// console.log(!!!('0' == 0)); //false
+// console.log(false == !false); //false
+//
+// console.log(1 != '1'); //true
+// console.log(1 !== '1'); //false
+//
+// console.log(1 != '0'); //true
